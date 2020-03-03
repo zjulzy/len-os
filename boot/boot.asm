d39bc51804ad0e58b2b1a9499401a3377853ab37
lizeyu <3077175187@qq.com>
2020-03-03 22:40:19 +0800

    建立loader

--- boot/boot.asm
+++ boot/boot.asm
@@ -4,7 +4,33 @@
 ; --------------------------------------------------------------------
 
 org	07c00h                               ;提醒bios将引导加载到0x07c00处，由BIOS决定，与cpu无关，07c00即32KB最早IBM的PC
-jmp boot_start
+jmp boot
+
 %include "boot.inc"
+;Ext2磁盘相关信息
 %include "Ext2.inc"
 %include "boot_load.asm"
+
+    ;硬盘数据结构
+    disk_load_packet:
+
+
+    
+    ;规定loader名称
+    Loader_Target db "loader.bin"
+    Loader_Length equ $-Loader_Target
+
+boot:
+    ;读取栈底地址
+    mov ax, cs
+    mov ds, cs
+    mov es ,cs
+    mov ss, cs
+    mov sp, Stack_Base
+
+    ;输出配置信息
+    call  clear_screen
+    mov dh, 0
+    call  display_str
+    call  sector_reader
+
--- boot/include/boot_load.asm
+++ boot/include/boot_load.asm
@@ -1,6 +1,14 @@
 ; ---------------------------------------------------------------------------------------
 ; 记录MBR引导过程中的一些操作
 ; ---------------------------------------------------------------------------------------
+
+Booting_Message:
+        db  "Booting...      "
+        db  "Fa Xian Loader  "
+        db "Loader Not Found"
+
+    Booting_Message_Length equ 16               ;打印状态信息长度相同
+
 read_section:
 
 ;清屏操作
@@ -13,3 +21,24 @@ clear_screen:
 	int 10h
 	popa                    ;出栈
 	ret
+
+;输出当前状态信息
+display_str:
+    pusha                ;入栈
+    mov ax , Booting_Message_Length
+    mul dh
+    add ax , Booting_Message
+    mov bp ,ax 
+    mov ax , ds
+    mov es , ax
+    mov cx , Booting_Message_Length
+    mov ax, 0x0130h
+    mov bx, 0x07h
+    mov dl, 0
+    int 10h
+    popa                  ;出栈
+    ret
+
+; --------------------------------------------------------------------------
+; 根据inode加载file
+; --------------------------------------------------------------------------
