5885eb1405f9622f8a1b5a67225e600eb176a487
lizeyu <3077175187@qq.com>
2020-04-01 22:29:04 +0800

    创建loader

--- boot/loader.asm
+++ boot/loader.asm
@@ -15,3 +15,20 @@ jmp loader_initial
 %include "pm.inc"
 %include "loader.inc"
 %include "gdt.asm"
+
+; **********************************************************************
+; GDT相关数据结构
+; GDT描述符表
+;                                                                          段基址               段界限         属性
+LABEL_GDT:			Descriptor            				   0,                    	0, 					0																	; 空描述符
+LABEL_DESC_FLAT_C:		Descriptor  		  0,              0fffffh, 			DA_CR  | DA_32 | DA_LIMIT_4K				; 0 ~ 4G(靠偏移寻址)
+LABEL_DESC_FLAT_RW:	 Descriptor             0,              0fffffh, 			DA_DRW | DA_32 | DA_LIMIT_4K			; 0 ~ 4G(靠偏移寻址)
+LABEL_DESC_VIDEO:		 Descriptor	 0B8000h,              0ffffh, 			DA_DRW | DA_DPL3									; 显存首地址
+GdtLen		equ	$ - LABEL_GDT                        ;GDT长度
+
+GdtPtr:
+		dw	GdtLen - 1														; GDT段界限
+		dd	Loader_Phy_Address + LABEL_GDT		  ; GDT段基址
+
+
+; **********************************************************************
