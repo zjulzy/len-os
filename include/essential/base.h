2387dd34b4793177899bd74d134328cd33e0f89a
lizeyu <3077175187@qq.com>
2020-05-02 17:15:18 +0800

    摸

--- include/essential/base.h
+++ include/essential/base.h
@@ -7,6 +7,21 @@
 #include "memory.h"
 #include "const.h"
 
-
+void hwint00();
+void hwint01();
+void hwint02();
+void hwint03();
+void hwint04();
+void hwint05();
+void hwint06();
+void hwint07();
+void hwint08();
+void hwint09();
+void hwint10();
+void hwint11();
+void hwint12();
+void hwint13();
+void hwint14();
+void hwint15();
 
 #endif
